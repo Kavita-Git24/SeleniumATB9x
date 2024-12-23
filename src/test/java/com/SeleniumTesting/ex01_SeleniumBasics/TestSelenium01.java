5ea6728f3ee6de5142cf7ea1e127d16f3253a91b
Kavita-24 <er.kavitasi@gmail.com>
2024-12-23 14:27:58 +0530

    Initial Commit

--- src/test/java/com/SeleniumTesting/ex01_SeleniumBasics/TestSelenium01.java
+++ src/test/java/com/SeleniumTesting/ex01_SeleniumBasics/TestSelenium01.java
@@ -1,4 +1,16 @@
 package com.SeleniumTesting.ex01_SeleniumBasics;
 
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
 public class TestSelenium01 {
+    @Test
+    public void test_Selenium()
+    {
+        EdgeDriver edgeDriver=new EdgeDriver();
+        edgeDriver.get("https://app.vwo.com");
+        System.out.println(edgeDriver.getTitle());
+    }
+
+
 }
